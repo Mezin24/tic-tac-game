47beecf87d911fc3fc5e308adbe56bc2c1f8a293
Pavel Mezencev <mezencev24@mail.ru>
2023-04-18 22:11:43 +0300

    add storage listener

--- vanile-js/js/main.js
+++ vanile-js/js/main.js
@@ -33,6 +33,10 @@ function init() {
     view.viewFields(store.game.moves);
   }
 
+  window.addEventListener('storage', () => {
+    initView();
+  });
+
   initView();
 
   view.bindNewRoundEvent(() => {
